330b8b982d59cd38f335f16d7b55ce44c59bf0a3
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-06-06 18:46:45 +0200

    added a pair of asserts

--- StringCalculator/py/tests.py
+++ StringCalculator/py/tests.py
@@ -7,9 +7,11 @@ class StringCalculatorTest(unittest.TestCase):
 
 	def test_add_one_number(self):
 		self.assertEqual(string_calculator.add('1'), 1, 'add("1") == 1')
+		self.assertEqual(string_calculator.add('6'), 6, 'add("6") == 6')
 
 	def test_add_two_numbers(self):
 		self.assertEqual(string_calculator.add('1,2'), 3, 'add("1,2") == 3')
+		self.assertEqual(string_calculator.add('5,3'), 8, 'add("5,3") == 8')
 
 
 if __name__ == '__main__':
