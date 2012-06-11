0227fa7dcebc5a8035c67a713faae032ed9d9229
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-06-11 17:34:59 +0200

    added handling of new line separator

--- StringCalculator/py/string_calculator.py
+++ StringCalculator/py/string_calculator.py
@@ -1,4 +1,4 @@
 def add(numbers):
-	numbers_list = [int(x) for x in numbers.split(',') if x]
+	numbers_list = [int(y) for x in numbers.split('\n') if x for y in x.split(',')]
 
 	return sum(numbers_list)
