051f77abf47e9f7260b186cbf5bd8ed4b6b325e0
Pedro Nofuentes <pedro.nofuentes@gmail.com>
2012-06-13 18:56:26 +0200

    added raise ValueError with negative numbers

--- StringCalculator/py/string_calculator.py
+++ StringCalculator/py/string_calculator.py
@@ -1,5 +1,12 @@
 def add(numbers):
+	numbers_list = []
 	if numbers.find('//') == 0:
-		return sum([int(x) for x in numbers[4:].split(numbers[2])])
+		numbers_list = [int(x) for x in numbers[4:].split(numbers[2])]
+	else:
+		numbers_list = [int(y) for x in numbers.split('\n') if x for y in x.split(',')]
 
-	return sum([int(y) for x in numbers.split('\n') if x for y in x.split(',')])
+	negative_numbers = [x for x in numbers_list if x < 0]
+	if negative_numbers:
+		raise ValueError('Negative numbers are not allowed: %s' % ','.join(['%i' % x for x in negative_numbers]))
+
+	return sum(numbers_list)
